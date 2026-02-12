3105adfbd6d8f69097618b3226a51f115070234f
bhuvafarin1997-netizen <bhuvafarin1997@gmail.com>
2026-02-12 21:36:52 +0000

    Update app.js

--- app.js
+++ app.js
@@ -68,4 +68,5 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+server.listen(PORT, () => console.log("Server is running"));
+
